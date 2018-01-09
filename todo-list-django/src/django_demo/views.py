383cdb3ee1eb8c48a1afe4de8c707632b235e1aa
Sanghee Lee <sanghee911@hotmail.com>
2018-01-10 00:14:31 +0900

    fixed nginx config

--- todo-list-django/src/django_demo/views.py
+++ todo-list-django/src/django_demo/views.py
@@ -5,13 +5,12 @@ import socket
 
 class IndexTemplateView(TemplateView):
     template_name = "index.html"
-
     frontend_ip = os.environ.get('FRONTEND_IP', 'localhost:8080')
     pgadmin_ip = os.environ.get('PGADMIN_IP', 'localhost:8001')
-
 
     def get_context_data(self, **kwargs):
         context = super(IndexTemplateView, self).get_context_data(**kwargs)
         context['frontend_ip'] = self.frontend_ip
         context['hostname'] = socket.gethostname()
+        context['pgadmin_ip'] = self.pgadmin_ip
         return context
